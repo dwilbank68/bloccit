b631d4a1acb782b68c03b7801157a006e9f83313
David Wilbanks <dwilbank@gmail.com>
2014-09-11 21:19:29 -0700

    finish devise

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,10 @@ if Rails.env.development?
       address:        'smtp.sendgrid.net',
       port:           '587',
       authentication: :plain,
-      user_name:      ENV['app29446464@heroku.com'],
-      password:       ENV['pibk1ecm'],
+      # user_name:      ENV['app29446464@heroku.com'],
+      # password:       ENV['pibk1ecm'],
+      user_name:      ENV['SENDGRID_USERNAME'],
+      password:       ENV['SENDGRID_PASSWORD'],
       domain:         'heroku.com',
       enable_starttls_auto: true
   }
