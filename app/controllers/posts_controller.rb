d3d9076f4714364ad6d38673fb4761a5d06ac551
David Wilbanks <dwilbank@gmail.com>
2014-09-14 16:14:40 -0700

    fixed posts update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,11 +33,12 @@ class PostsController < ApplicationController
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title,:body))
       flash[:notice] = "Post was saved."
-      redirect_to [@topic,@post]
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
