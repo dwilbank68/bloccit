02e56f89e6f22d43cd35cbc0c2b4bcc04207f56c
David Wilbanks <dwilbank@gmail.com>
2014-09-16 16:10:00 -0700

    post markdown working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
-  def form_group_tag(errors,&block)
+  def errors?(errors)
+    puts "errors is #{errors.inspect}"
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
+      'form-group has-error'
     else
-      content_tag :div, capture(&block), class: 'form-group'
+      'form-group'
     end
   end
 
