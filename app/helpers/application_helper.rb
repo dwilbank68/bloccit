8b77191bec54507fc0b376d055677972f3868099
David Wilbanks <dwilbank@gmail.com>
2014-09-26 18:11:08 -0700

    vote-link-classes working using 'send'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,38 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+  # This is here in case some says I can't use 'send'
+  
+  # def vote_link_classes(post, dir)
+  #   if dir == 'up'
+  #
+  #     if current_user.voted(post) && current_user.voted(post).up_vote?
+  #       return "glyphicon glyphicon-chevron-up voted"
+  #     else
+  #       return "glyphicon glyphicon-chevron-up"
+  #     end
+  #
+  #   elsif dir == 'down'
+  #
+  #     if current_user.voted(post)&& current_user.voted(post).down_vote?
+  #       return "glyphicon glyphicon-chevron-down voted"
+  #     else
+  #       return "glyphicon glyphicon-chevron-down"
+  #     end
+  #
+  #   else
+  #     return "see if /views/votes/_voter.html.erb gave you something other than 'up' or 'down'"
+  #   end
+  #
+  # end
+
+  def vote_link_classes(post,dir)
+    if current_user.voted(post) && current_user.voted(post).send("#{dir}_vote?")
+      "glyphicon glyphicon-chevron-#{dir} voted"
+    else
+      "glyphicon glyphicon-chevron-#{dir}"
+    end
+  end
 
 end
 
