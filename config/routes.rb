5e9d639462ab8657d2ff9ea6450f72f7c0e08c6a
David Wilbanks <dwilbank@gmail.com>
2014-09-28 22:43:32 -0700

    not working - I think it is topics_controller#show - the #includes(:comment) part... When I take it out, things work

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :update, :index]
 
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'posts/new'
-  # get 'posts/edit'
-  # resources :posts # - replaces the above four lines
-  # resources :topics do
-  #   resources :posts, except: [:index] do
-  #     resources :comments, only: [:create, :destroy]
-  #   end
-  # end
-  # becomes shallow nesting below
+  resources :posts, only: [:index]
 
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index], controller: 'topics/posts' do
+      resources :comments, only: [:create, :destroy]
+      resources :favorites, only: [:create, :destroy]
+
+      post '/up-vote'   => 'votes#up_vote',   as: :up_vote
+      post '/down-vote' => 'votes#down_vote', as: :down_vote
+    end
   end
-
-  resources :posts, only: [] do
-    resources :comments, only: [:create, :destroy]
-    resources :favorites, only: [:create, :destroy]
-
-    post '/up-vote'   => 'votes#up_vote',   as: :up_vote
-    post '/down-vote' => 'votes#down_vote', as: :down_vote
-
-  end
-
 
   get 'about' => 'welcome#about'
 
