2e84600fae681344fe827c4bb754017e203dde41
David Wilbanks <dwilbank@gmail.com>
2014-09-29 21:30:18 -0700

    added ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+  respond_to :html, :js
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
@@ -25,11 +25,14 @@ class CommentsController < ApplicationController
 
     if @comment.destroy
       flash[:notice] = "Comment successfully deleted."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Error deleting comment."
-      redirect_to [@topic, @post]
     end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
+    end
+
   end
 
   private
